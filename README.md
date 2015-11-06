1e507f35583b570a176d044ad122924679090bfa
egoist <aprilorange.net@icloud.com>
2015-11-06 16:53:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# ogh
+
+**Open GitHub Page** for your repo directly in Terminal.
+
+```bash
+➜  ~  npm install -g ogh
+# in your project root, type `ogh` and press `return`
+➜  localdb git:(master) ogh
+# then you default browser
+# will open `https://github.com/egoist/localdb` for you
+```
+
+## License
+
+MIT.
---
+++ index.js
@@ -0,0 +1,23 @@
+var fs = require('fs')
+var ini = require('ini')
+var pathExists = require('path-exists')
+var log = require('typelog')
+var open = require('open')
+var parse = require('git-url-parse')
+
+module.exports = function () {
+  var configFile = process.cwd() + '/.git/config'
+  if(!pathExists.sync(configFile)) {
+    return log.error('Not a git repository')
+  }
+  var config = ini.parse(fs.readFileSync(configFile, 'utf-8'))
+  var remote = config['remote "origin"']
+  if (!remote) {
+    return log.error('Remote origin does not exist')
+  }
+  var url = parse(remote.url)
+  if (url.source !== 'github.com') {
+    return log.warn('This is not a GitHub repository')
+  }
+  open('https://github.com/' + url.owner + '/' + url.name)
+}
